4a05d6688b9f31705d1676c3994ed34e24c02284
Murat Menzilci <menzilcim>
2021-08-09 13:41:18 +0300

    City class is ready!

---
+++ citiesExample/Citites.swift
@@ -0,0 +1,16 @@
+//
+//  Citites.swift
+//  citiesExample
+//
+//  Created by Murat Menzilci on 9.08.2021.
+//
+
+import Foundation
+import UIKit
+
+class Cities{
+    
+    var city = ""
+    var country = ""
+    var img = UIImage()
+}
---
+++ citiesExample/DetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsViewController.swift
+//  citiesExample
+//
+//  Created by Murat Menzilci on 9.08.2021.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
